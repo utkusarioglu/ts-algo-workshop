43bf0a221f3b7ee52bc2ed26a194979caae7cb05
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-09-05 10:05:51 +0000

    Refactor: Implement review notes

--- src/sorting-algorithms/insertion-sort.ts
+++ src/sorting-algorithms/insertion-sort.ts
@@ -1,9 +1,9 @@
 export class InsertionSort {
   public loop(unsorted: number[]): number[] {
     for (let i = 1; i < unsorted.length; i++) {
-      inner: for (let j = i; j > 0; j--) {
+      for (let j = i; j > 0; j--) {
         if (unsorted[j - 1]! < unsorted[j]!) {
-          continue inner;
+          continue;
         }
         const temp: number = unsorted[j]!;
         unsorted[j] = unsorted[j - 1]!;
